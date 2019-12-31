c9bc3bc6a3779cf8a84d7e6d45639f02de5cb68e
xi <xi@qq.com>
2019-12-31 15:42:22 +0800

    first commit

---
+++ src/igeek/Main.java
@@ -0,0 +1,14 @@
+package igeek;
+
+/**
+ * @Author : 奚晨玲
+ * @Date :2019/12/31 15:36
+ * @Description :
+ * @Modify By : Admin
+ * @Version :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
